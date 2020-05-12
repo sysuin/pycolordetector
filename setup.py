6089f1ea42ef06b53ff06a34bf4fc309e612f752
Sunny Singh <sunny@doeity.com>
2020-05-12 08:35:45 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,20 @@
 from setuptools import find_packages, setup
 
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(name='pycolordetector',
       packages=find_packages(include=['pycolordetector']),
       version='0.0.1',
       description='A simple library for detecting colors in images',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       author='Sunny Singh',
       author_email='sunnysinghnitb@gmail.com',
+      license='MIT',
       url='https://github.com/sunnysinghnitb/pycolordetector',
       classifiers=[
         "Programming Language :: Python :: 3",
@@ -13,4 +22,8 @@ setup(name='pycolordetector',
         "Operating System :: OS Independent",
       ],
       python_requires='>=3.6',
+      project_urls={
+        'Funding': 'https://donate.pypi.org',
+        'Documentation': r'https://pycolordetector.readthedocs.io/en/latest/'
+      },
       )
